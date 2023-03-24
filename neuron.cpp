7a38a4c727664ed1134bc2415363c89a11ccdd1a
DonaldoAyala <ayala.segoviano.donaldo@gmail.com>
2023-03-24 04:45:08 -0600

    Fixed training method by updating the bias

--- neuron.cpp
+++ neuron.cpp
@@ -9,13 +9,12 @@ using namespace std;
     Classs for modeling neural networks.
     Current version only allows a single layer.
 */
-class NeuralNetwork 
+class Perceptron 
 {
     private:
         // weights[i][j] denote weight for input j of neuron i
-        vector<vector<float>> weights;
-        vector<float> bias;
-        int neurons;
+        vector<float> weights;
+        float bias;
         int inputs;
 
         float hardlim(float n) 
@@ -24,78 +23,61 @@ class NeuralNetwork
         }
 
     public:
-        NeuralNetwork(int neurons, int inputs) 
+        Perceptron(int inputs, float bias) 
         {
-            this -> neurons = neurons;
             this -> inputs = inputs;
-            weights.resize(neurons, vector<float>(inputs, 0.0f));
-            bias.resize(neurons);
-        }
-
-        void printWeights() 
-        {
-            for (auto neuron: weights)
-            {
-                for (auto weight : neuron)
-                {
-                    cout << weight << " ";
-                }
-                cout << endl;
-            }
-        }
-
-        NeuralNetwork(vector<vector<float>> weights, vector<float> bias) 
-        {
-            this -> weights = weights;
+            weights.resize(inputs);
             this -> bias = bias;
-            neurons = weights.size();
-            inputs = weights.size() > 0 ? weights[0].size() : 0;
         }
 
-        vector<float> process(vector<float> input) 
+        void print() 
         {
-            vector<float> output(neurons, 0);
-
-            for (int i = 0; i < neurons; i++)
+            cout << "Weights: ";
+            for (auto x: weights)
             {
-                for (int j = 0; j < inputs; j++)
-                {
-                    output[i] += weights[i][j] + bias[i];
-                }
-                output[i] = hardlim(output[i]);
+                cout << x << " ";
             }
+            cout << " Bias: " << bias;
+            cout << endl;
+        }
+
+        float process(vector<float> input)
+        {
+            float output = 0;
+
+            for (int i = 0; i < inputs; i++)
+            {
+                output += weights[i] * input[i];
+            }
+            output += bias;
+            output = hardlim(output);
 
             return output;
         }
 
-        void setWeight(int neuron, int input, float value) 
+        void setWeight(int input, float value) 
         {
-            weights[neuron][input] = value;
+            weights[input] = value;
         }
 
-        void setBias(int neuron, float bias) 
+        void setBias(float bias) 
         {
-            this -> bias[neuron] = bias;
+            this -> bias = bias;
         }
 
-        float getWeight(int neuron, int input) 
+        float getWeight(int input) 
         {
-            return weights[neuron][input];
+            return weights[input];
         }
 
-        float getBias(int neuron) 
+        float getBias()
         {
-            return bias[neuron];
+            return bias;
         }
 
         int numInputs() 
         {
             return this -> inputs;
-        }
-
-        int numNeurons() 
-        {
-            return this -> neurons;
         }
 };
 
@@ -104,35 +86,36 @@ class Trainer
 
     public:
 
-        void train(NeuralNetwork* neuralNetwork, int iterations, float learningRate, vector<pair<vector<float>, float>> trainingSet)
+        void train(Perceptron* perceptron, int iterations, float learningRate, vector<pair<vector<float>, float>> trainingSet)
         {
             while (iterations--)
             {
-                neuralNetwork -> printWeights();
-                vector<float> output;
+                // perceptron -> print();
+                float output;
                 for (pair<vector<float>, float> example: trainingSet) 
                 {
                     vector<float> input = example.first;
                     float expectedOutput = example.second;
 
-                    output = neuralNetwork -> process(input);
+                    output = perceptron -> process(input);
+                    float error = expectedOutput - output;
 
-                    for (int o = 0; o < neuralNetwork -> numNeurons(); o++)  
+                    // Update weights
+                    for (int i = 0; i < perceptron -> numInputs(); i++)
                     {
-                        for (int i = 0; i < neuralNetwork -> numInputs(); i++) 
-                        {
-                            float newWeight = neuralNetwork -> getWeight(o, i) + learningRate * (expectedOutput - output[o]) * input[i];
-                            neuralNetwork -> setWeight(o, i, newWeight);
-                        }
+                        float newWeight = perceptron -> getWeight(i) + learningRate * (error) * input[i];
+                        perceptron -> setWeight(i, newWeight);
                     }
+
+                    // Update bias
+                    perceptron -> setBias(perceptron -> getBias() + error * learningRate);
+
                 }
             }
         }
 };
 
 int main () {
-    Trainer* trainer = new Trainer();
-    NeuralNetwork* andNN = new NeuralNetwork(1, 2);
     vector<pair<vector<float>, float>> andTrainingSet = 
     {
         {{0, 0}, 0},
@@ -140,16 +123,38 @@ int main () {
         {{1, 0}, 0},
         {{1, 1}, 1},
     };
+    vector<pair<vector<float>, float>> orTrainingSet = 
+    {
+        {{0, 0}, 0},
+        {{0, 1}, 1},
+        {{1, 0}, 1},
+        {{1, 1}, 1},
+    };
+    vector<pair<vector<float>, float>> xorTrainingSet = 
+    {
+        {{0, 0}, 0},
+        {{0, 1}, 1},
+        {{1, 0}, 1},
+        {{1, 1}, 0},
+    };
 
-    trainer -> train(andNN, 10, 0.01f, andTrainingSet);
+    Trainer* trainer = new Trainer();
+    Perceptron* andPerceptron = new Perceptron(2, 0);
+    Perceptron* orPerceptron = new Perceptron(2, 0);
+    Perceptron* xorPerceptron = new Perceptron(2, 0);
 
-    cout << "AND" << endl;
+    trainer -> train(andPerceptron, 10, 0.1f, andTrainingSet);
+    trainer -> train(orPerceptron, 10, 0.1f, orTrainingSet);
+    trainer -> train(xorPerceptron, 10, 0.1f, xorTrainingSet);
+    
+    printf("Input \t\tAND \tOR\t XOR\n");
     for (auto test : andTrainingSet) 
     {
         auto input = test.first;
-        auto result = andNN -> process(input);
-        cout << result.size() << endl;
-        cout << input[0] << " " << input[1] << " = " << result[0] << endl;
+        float andResult = andPerceptron -> process(input);
+        float orResult = orPerceptron -> process(input);
+        float xorResult = xorPerceptron -> process(input);
+        printf("%.1f  %.1f \t%.1f \t%.1f \t%.1f\n", input[0], input[1], andResult, orResult, xorResult); 
     }
     return 0;
 }
